3404e40048446f7af5302a8f093d093c0a2d30cb
p1k0pan <645758634@qq.com>
2021-10-07 23:09:03 +0800

    wallpaper

--- autostart.sh
+++ autostart.sh
@@ -6,8 +6,8 @@
 #please replace ./goblocks.go to ~/goblocks
 cd ~/goblocks; ./goblocks & 
 bash ~/scripts/chKeymap.sh
-bash ~/scripts/wp-change.sh
-# /bin/bash ~/scripts/wp-autochange.sh &
+# bash ~/scripts/wp-change.sh
+/bin/bash ~/scripts/wp-autochange.sh &
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 # picom -b
 bash ~/scripts/tap-to-click.sh &
--- wp-change.sh
+++ wp-change.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-feh --recursive --randomize --bg-fill ~/.local/share/wallpapers/wallhaven-57rq25.jpg
+# feh --recursive --randomize --bg-fill ~/.local/share/wallpapers/wallhaven-57rq25.jpg
+feh --recursive --randomize --bg-fill ~/.local/share/wallpapers
 #feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
