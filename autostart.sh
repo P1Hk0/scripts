d7391a19aa3e543b759d2b3907e328e09c36131b
p1k0pan <645758634@qq.com>
2021-11-12 11:03:53 +0800

    autostart add picom

--- autostart.sh
+++ autostart.sh
@@ -9,7 +9,7 @@ bash ~/scripts/chKeymap.sh
 # bash ~/scripts/wp-change.sh
 /bin/bash ~/scripts/wp-autochange.sh &
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-# picom -b
+picom -b
 bash ~/scripts/tap-to-click.sh &
 bash ~/scripts/inverse-scroll.sh &
 nm-applet &
